5efc5ed3718029732e8ca373a420402395d49bde
Nadia Aymbinderow <nadiaaymbin@gmail.com>
2020-07-20 11:48:48 +0300

    delete favorite components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 import CreatePostForm from './features/components/form.component';
 import Button from './features/components/button.component';
 import RenderPostList from './features/components/post.list.component';
-import Images from './features/components/image.list.component';
+import ImageList from './features/components/image.list.component';
+import Card from './features/components/card.component';
 
 
 function App() {
@@ -36,15 +37,13 @@ function App() {
       .then(fetchPost)
   };
 
-  const onCreatePost = (e) => {
-    createPost({ message: e })
+  const onCreatePost = (e, imgId) => {
+    createPost({ message: e, imgId})
   }
 
   return (
     <div className="App">
-      {/* <Images imageList={images}></Images> */}
-      <Images imageList={images} />
-      <CreatePostForm onCreatePost={onCreatePost} />
+      <ImageList imageList={images} posts={posts} onCreatePost={onCreatePost} />
       <Button x={() => console.log("home clicked")} text='home' />
       <RenderPostList posts={posts} />
     </div>
---
+++ frontend/src/features/components/card.component.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+import RenderPostList from './post.list.component';
+import CreatePostForm from './form.component'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        maxWidth: 345,
+    },
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    },
+
+}));
+
+export default function RecipeReviewCard(props) {
+    const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
+    return (
+        <Card className={classes.root}>
+            <CardHeader />
+            <CardMedia
+                className={classes.media}
+                image={props.image.url}
+            />
+            <CardActions disableSpacing>
+                <IconButton aria-label="add to favorites"
+                >
+                    <FavoriteIcon />
+                </IconButton>
+                <IconButton aria-label="share">
+                    <ShareIcon />
+                </IconButton>
+                <IconButton
+                    className={clsx(classes.expand, {
+                        [classes.expandOpen]: expanded,
+                    })}
+                    onClick={() => {
+                        handleExpandClick();
+                    }}
+
+                    aria-expanded={expanded}
+                    aria-label="show more"
+                >
+                    <ExpandMoreIcon />
+                </IconButton>
+                <CreatePostForm onCreatePost={props.onCreatePost} image={props.image} />
+            </CardActions>
+            <Collapse in={expanded} timeout="auto">
+                <CardContent>
+                    <RenderPostList posts={props.posts} />
+                </CardContent>
+            </Collapse>
+        </Card>
+    );
+}
